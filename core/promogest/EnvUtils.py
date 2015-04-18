c7b0b795156413484634d6dd56995df2db45f401
m3nt0r3@gmail.com <m3nt0r3@gmail.com@c8087da8-80d7-f218-d0bb-7d131f7cdc9e>
2015-04-18 12:00:55 +0000

    molto clean del codice , rifatto laytout stoccaggio e movimento magazzino

--- core/promogest/EnvUtils.py
+++ core/promogest/EnvUtils.py
@@ -195,7 +195,7 @@ def _send(fromaddr=None, total_addrs=None, msg=None):
         server.ehlo()
         server.starttls()
         server.ehlo()
-        server.login("promogestlogs@gmail.com", "pr0m0t0x4")
+        server.login("promogestlogs@gmail.com", "pr0m0t0x5")
         server.sendmail("promogestlogs@gmail.com",
                     "promogestlogs@gmail.com",
                         msg.as_string())
