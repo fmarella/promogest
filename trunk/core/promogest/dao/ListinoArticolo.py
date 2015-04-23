fdc89d37c873c68644893a1491f0865d0de30014
m3nt0r3@gmail.com <m3nt0r3@gmail.com@c8087da8-80d7-f218-d0bb-7d131f7cdc9e>
2015-04-23 18:10:48 +0000

    fix promowear ...Env list

--- trunk/core/promogest/dao/ListinoArticolo.py
+++ trunk/core/promogest/dao/ListinoArticolo.py
@@ -156,7 +156,7 @@ class ListinoArticolo(Base, Dao):
         else:
             return ""
 
-    if (hasattr(conf, "PromoWear") and getattr(conf.PromoWear, 'mod_enable') == "yes"):
+    if (hasattr(conf, "PromoWear") and getattr(conf.PromoWear, 'mod_enable') == "yes") or "PromoWear" in Environment.modulesList:
         @property
         def denominazione_gruppo_taglia(self):
             if self.arti:
